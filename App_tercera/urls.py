d88c09bcc364f45bb2d57baccd95a50b107a534f
Sebastian Palomino <sebastianpalomino67@yahoo.es>
2024-04-08 21:25:10 -0300

    Mensaje descriptivo de tus cambios

--- App_tercera/urls.py
+++ App_tercera/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio , name="home"),
-    path("profesores/", views.lista_profesores, name="lista_profesores"),
     path("ver_cursos", views.ver_cursos , name="cursos"),
     #path("alta_curso/<nombre>", views.alta_curso),
     path("alumnos", views.alumnos , name="alumnos"),
